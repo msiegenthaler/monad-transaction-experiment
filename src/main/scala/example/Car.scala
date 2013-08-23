f649db28edcbe94c680286d37f6283e8ecbe473b
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2013-08-23 10:31:08 +0200

    implement Car Repository using anorm.

--- src/main/scala/example/Car.scala
+++ src/main/scala/example/Car.scala
@@ -1,32 +1,35 @@
 package example
 
+import anorm._
+import anorm.SqlParser._
 import mt._
 
 case class CarId(id: Long)
 case class Car(id: CarId, name: String)
 
 trait CarRepository {
-  def all = Jdbc { c =>
-    List(Car(CarId(10), "Ford Mustard"))
+  def all = Jdbc { implicit c =>
+    SQL("SELECT * FROM car").as(car *)
   }
 
-  def byName(name: String) = all.map(_.filter(_.name == name))
-
-  def byId(id: CarId) = Jdbc { c =>
-    val s = c.prepareStatement("SELECT name FROM car WHERE id=?")
-    s.setLong(1, id.id)
-    val rs = s.executeQuery()
-    if (rs.next) Some(Car(id, rs.getString(1)))
-    else None
+  def byName(name: String) = Jdbc { implicit c =>
+    SQL("SELECT * FROM car WHERE name = {name}").on("name" -> name).as(car *)
   }
 
-  def add(name: String) = Jdbc { c =>
-    // add the stuff
-    CarId(100)
+  def byId(id: CarId) = Jdbc { implicit c =>
+    SQL("SELECT * FROM car WHERE id = {id}").on("id" -> id.id).as(car.singleOpt)
   }
 
-  def remove(id: CarId) = Jdbc { c =>
-    //remove
+  def add(name: String) = Jdbc { implicit c =>
+    SQL("INSERT INTO car(name) VALUES({name})").on("name" -> name).executeInsert().map(CarId.apply)
+  }
+
+  def remove(id: CarId) = Jdbc { implicit c =>
+    SQL("DELETE FROM car WHERE id={id}").on("id" -> id.id).executeUpdate()
     ()
+  }
+
+  private val car = get[Long]("id") ~ get[String]("name") map {
+    case id ~ name => Car(CarId(id), name)
   }
 }
